ad9ed7ca2f06fb0681c44bb8a0ad121323c8ba65
Sergio Hernandez <sergio.medrano-ext@ab-inbev.com>
2024-08-08 16:33:00 -0600

    changes

---
+++ src/main/java/com/readymind/TestPackagesApplication.java
@@ -0,0 +1,13 @@
+package com.readymind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPackagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPackagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readymind/TestPackagesApplicationTests.java
@@ -0,0 +1,10 @@
+package com.readymind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+class TestPackagesApplicationTests {
+
+
+}
